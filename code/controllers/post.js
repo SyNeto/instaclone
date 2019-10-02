4577d270adb367d0e93295e85a3bd726a6bbd3bd
Ernesto Jimenez V <ernesto@bedu.org>
2019-10-01 19:36:45 -0500

    Added error handling

--- code/controllers/post.js
+++ code/controllers/post.js
@@ -1,5 +1,11 @@
 const PostModel = require('../models/post');
 
-exports.list = async (req, res) => {
-    return res.json(await PostModel.find().exec());
+exports.list = async (req, res, next) => {
+    try {
+        throw new Error('Un error random...');
+        return res.json(await PostModel.find().exec());
+    } catch(error) {
+        next(error);
+    }
+    
 }
